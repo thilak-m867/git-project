1f36289ce8a00f21275b14a5a59440096b3424da
thilak-m867 <daddylonglegs867@gmail.com>
2020-12-31 20:29:42 +0530

    message

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,7 +14,8 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+     
+    platform1 = new Ground(200,200,20,20)
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    platform1.display();
 }
